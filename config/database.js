69f1cd2bba7da5d6728694bb018af06ad521b5a5
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2024-10-02 11:04:08 +0200

    Deploy to Vercel

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
 const Pool = require('pg').Pool;
 
+
 const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+/* const pool = new Pool({
   user: 'postgres',
   password: 'admin',
   host: 'localhost',
   port: 5432,
   database: 'auction',
-});
+}); */
 
 module.exports = pool;
